85efaa20fe7b4977f2e620866067c3a0efe5e25b
Jinto Jose <jintoppy@gmail.com>
2018-05-30 10:39:13 +0530

    adding view detail option

---
+++ src/app.constant.js
@@ -0,0 +1,7 @@
+angular.module('angular-shopping-cart')
+    .constant('AppConstant', {
+        API: {
+            BASE_URL: 'http://5b0cfb6e8126c900149974fe.mockapi.io/',
+            PRODUCTS: 'products'
+        }
+    });
--- src/catalog/catalog.service.js
+++ src/catalog/catalog.service.js
@@ -1,6 +1,10 @@
 var catalog = angular.module('angular-shopping-cart.catalog');
-catalog.service('CatalogService', ['$http', function($http){
+catalog.service('CatalogService', ['$http', 'AppConstant', 
+function($http, AppConstant){
     this.getProducts = function(){
-        return $http.get('http://5b0cfb6e8126c900149974fe.mockapi.io/products');
+        return $http.get(AppConstant.API.BASE_URL + AppConstant.API.PRODUCTS);
     };
+    this.getProductById = function(id){
+        return $http.get(AppConstant.API.BASE_URL + AppConstant.API.PRODUCTS + '/' + id);
+    }
 }]);
